aeccce296bc72ea7a1ac2c05c67f13f20b469a8c
hhh0pE <vlad@lesnoy.name>
2015-04-13 23:35:12 +0300

    Added static files server. For css, js and other static files.

--- main.go
+++ main.go
@@ -10,19 +10,26 @@ import (
 )
 
 func render(name string, data interface{}, w http.ResponseWriter) {
-	t, err := template.ParseFiles("templates/" + name + ".tmpl")
+
+	t, err := template.ParseFiles("templates/"+name+".html", "templates/layout.html")
 	if err != nil {
 		panic("Error when parsing template `" + name + "`. Error message: " + err.Error())
 	}
 
-	t.Execute(w, data)
+	err = t.ExecuteTemplate(w, "layout", data)
+	if err != nil {
+		fmt.Println("Error when render page " + name + "; Error: " + err.Error())
+	}
 
 }
 
 func rootAction(w http.ResponseWriter, r *http.Request) {
 	post_model := models.Post{}
 	posts := post_model.GetAll()
-	render("index", posts, w)
+	render("index", struct {
+		Title string
+		Posts []models.Post
+	}{"Lesnoy's blog | main page", posts}, w)
 }
 
 func viewAction(w http.ResponseWriter, r *http.Request) {
@@ -46,13 +53,13 @@ func viewAction(w http.ResponseWriter, r *http.Request) {
 	}{post, prev_post_link, next_post_link}, w)
 }
 
-
 func main() {
 	fmt.Println("starting server..")
 	r := mux.NewRouter()
 
+	r.PathPrefix("/assets/").Handler(http.StripPrefix("/assets", http.FileServer(http.Dir("./assets/"))))
 	r.HandleFunc("/", rootAction)
-	r.HandleFunc("/{post_url}/", viewAction)
+	r.HandleFunc("/post/{post_url}/", viewAction)
 
 	err := http.ListenAndServe(":9000", r)
 	if err != nil {
